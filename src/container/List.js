3dc71a6550df5d460e193e9265f1a4104cce9292
Sephirokt <luismanuelpd@gmail.com>
2022-07-20 21:04:48 -0400

    Configurando pro-types

--- src/container/List.js
+++ src/container/List.js
@@ -1,24 +1,70 @@
-import React from "react";
+import React, {Fragment} from "react";
 
-import Card from '../components/Card/Card'
+import Card from '../components/Card/Card';
+
+const API = "https://www.omdbapi.com/?i=tt3896198&apikey=83ad705f";
 
 class List extends React.Component{
 
     constructor(){
         super();
         this.state = {
-            data: []
+            data: [],
+            searchTerm: "",
+            error: ""
         }
     }
 
    async componentDidMount(){
-      const res = await fetch(__dirname+'./assets/data.json');
+      //const res = await fetch(__dirname+'./assets/data.json');
+      const res = await fetch(`${API}&s=batman`);
       const resJSON = await res.json();
-      this.setState({data: resJSON});
+      console.log(resJSON);
+      this.setState({data: resJSON.Search});
+    }
+
+   async handleSubmit(e){
+        e.preventDefault();
+        if(!this.state.searchTerm){
+            return this.setState({error: 'Please write a valid text'})
+        }
+
+       const res = await fetch(`${API}&s=${this.state.searchTerm}`);
+       const data = await res.json();
+
+        if(!data){
+            return this.setState({error: 'There are no results'})
+        }
+
+       this.setState({data: data.Search, error: "", searchTerm: ""})
     }
 
     render(){
-        return this.state.data.map()
+        return( 
+            <Fragment>
+                <div className="row">
+                    <div className="col-md-4 offset-md-4 p-4">
+                        <form onSubmit={(e) => this.handleSubmit(e)}>
+                            <input 
+                                type="text" 
+                                className="form-control" 
+                                placeholder="Search" 
+                                onChange={ e => this.setState({searchTerm: e.target.value})}
+                                value={this.state.searchTerm} 
+                                autoFocus/>
+                        </form>
+                        <p className="text-white">{this.state.error ? this.state.error : ''}</p>
+                    </div>
+                </div>
+                <div className="row">
+                {
+                    this.state.data.map((movie, index) => {
+                        return <Card movie={movie} key={index}/>
+                    })
+                }
+                </div>
+            </Fragment>
+        )
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import ReactDOM from 'react-dom';
 import List from './container/List';
 
 import 'bootswatch/dist/lux/bootstrap.min.css';
 
-ReactDOM.render(<List/>, document.getElementById('root'));
+
+const App = () =>{
+    return (
+        <Fragment>
+            <nav className='navbar navbar-dark bg-dark border-bottom border-white'>
+                <a href='/' className='navbar-brand'>
+                    MovieApp
+                </a>
+            </nav>
+            <main className="bg-dark">
+                <div className='container'>
+                    <List/>
+                </div>
+            </main>
+        </Fragment>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
